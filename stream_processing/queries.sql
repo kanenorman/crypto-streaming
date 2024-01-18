bf4fc18ddcf5c644439ea724d9b344f8fbcf37b5
Kane Norman <51185594+kanenorman@users.noreply.github.com>
2024-01-18 23:09:30 +0000

    Document SQL queries

--- stream_processing/queries.sql
+++ stream_processing/queries.sql
@@ -1,9 +1,19 @@
-CREATE TABLE CryptoPrices (
-  c STRING,
-  p DECIMAL(18, 2),
-  s STRING,
-  t BIGINT,
-  v DECIMAL(18, 3)
+/*********************************
+  CREATE A KAFKA SOURCE TABLE
+
+  READS KAFKA TOPIC `crypto-prices`
+  AND CREATES A SOURCE TABLE WITH THE DATA
+  FROM THE TOPIC
+
+*********************************/
+
+-- Create a Kafka source table (CRYPTO_PRICES_BRONZE)
+CREATE TABLE CRYPTO_PRICES_BRONZE (
+  c STRING,              -- Trade conditions
+  p DECIMAL(18, 2),      -- Last price
+  s STRING,              -- Symbol
+  t BIGINT,              -- UNIX timestamp in milliseconds
+  v DOUBLE      -- Volume
 ) WITH (
   'connector' = 'kafka',
   'topic' = 'crypto-prices',
@@ -12,3 +22,19 @@ CREATE TABLE CryptoPrices (
   'properties.group.id' = 'crypto-price-consumer',
   'format' = 'json'
 );
+
+/*********************************
+  CREATE A VIEW FOR TRANSFORMED DATA
+
+  READS DATA FROM CRYPTO_PRICES_BRONZE TABLE
+  AND CREATES A VIEW WITH TRANSFORMED COLUMNS
+**********************************/
+
+CREATE VIEW CRYPTO_PRICES_SILVER  AS
+SELECT
+  c AS conditions,                    -- Trade conditions
+  p AS price,                         -- Last price
+  s AS symbol,                        -- Symbol
+  TO_TIMESTAMP_LTZ(t, 3) AS at_time,  -- Local timestamp with millisecond precision
+  v AS volume                         -- Volume
+FROM CRYPTO_PRICES_BRONZE;
