9bda694cc9c8e3aaaf693a01c37af3ed777deb08
Kane Norman <51185594+kanenorman@users.noreply.github.com>
2024-01-20 13:48:34 -0600

    Update queries.sql
    Formatting

--- flink/queries.sql
+++ flink/queries.sql
@@ -26,8 +26,8 @@ CREATE TABLE SOURCE_CRYPTO_PRICES (
 CREATE VIEW TRANSFORMED_CRYPTO_PRICES  AS
 SELECT
   p AS price,                               -- Last price       
-  SPLIT_INDEX(s, ':',0) AS exchange,        -- Trading exchange (e.g. Coinbase)
-  SPLIT_INDEX(s, ':',1) AS trading_pair,    -- Trading pair     (e.g. BTCUSD)
+  SPLIT_INDEX(s, ':', 0) AS exchange,        -- Trading exchange (e.g. Coinbase)
+  SPLIT_INDEX(s, ':', 1) AS trading_pair,    -- Trading pair     (e.g. BTCUSD)
   at_time,                                  -- Local timestamp with millisecond precision
   v AS volume                               -- Volume
 FROM SOURCE_CRYPTO_PRICES;
