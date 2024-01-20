39b1821ed249418dbebb0be64760b9e5c6b7c1d7
Kane Norman <51185594+kanenorman@users.noreply.github.com>
2024-01-20 13:50:31 -0600

    Delete flask directory

--- flask/app/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from .price_history import PriceHistory
--- flask/app/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-main = Blueprint("main", __name__)
-
-
-@main.route("/")
-def index():
-    return "Hello, World"
