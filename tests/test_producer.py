5194f9a6ca0016e2783af801969942c4fb03d535
kanenorman <kanenorman1996@gmail.com>
2024-01-20 12:31:54 -0600

    Update unit test

--- tests/test_producer.py
+++ tests/test_producer.py
@@ -5,26 +5,26 @@ import pytest
 from confluent_kafka import KafkaException, Producer
 from hamcrest import assert_that, equal_to, has_key, instance_of
 
-from kafka_producer import main
+from kafka import producer as service
 
 
 def test_get_producer_configurations_is_dict():
-    config = main._get_producer_configurations()
+    config = service._get_producer_configurations()
     assert_that(config, instance_of(dict))
 
 
 def test_get_producer_configurations_has_bootstrap_servers():
-    config = main._get_producer_configurations()
+    config = service._get_producer_configurations()
     assert_that(config, has_key("bootstrap.servers"))
 
 
 def test_get_producer_configurations_has_client_id():
-    config = main._get_producer_configurations()
+    config = service._get_producer_configurations()
     assert_that(config, has_key("client.id"))
 
 
 def test_get_producer_configurations_bootstrap_servers_value():
-    config = main._get_producer_configurations()
+    config = service._get_producer_configurations()
     assert_that(
         config["bootstrap.servers"],
         equal_to("broker1:9092,broker2:19092,broker3:29092"),
@@ -32,7 +32,7 @@ def test_get_producer_configurations_bootstrap_servers_value():
 
 
 def test_get_producer_configurations_client_id_value():
-    config = main._get_producer_configurations()
+    config = service._get_producer_configurations()
     assert_that(config["client.id"], equal_to("crypto-price-producer"))
 
 
@@ -43,10 +43,10 @@ async def test_create_kafka_producer():
     )
 
     with unittest.mock.patch(
-        "kafka_producer.main._get_producer_configurations",
+        "kafka.producer._get_producer_configurations",
         mock_get_producer_configurations,
     ):
-        producer = await main._create_kafka_producer()
+        producer = await service._create_kafka_producer()
 
         assert_that(producer, instance_of(Producer))
 
@@ -59,7 +59,7 @@ async def test_send_record_to_kafka():
 
     sample_record = {"spam": "eggs"}
 
-    await main._send_record_to_kafka(sample_record, mock_producer)
+    await service._send_record_to_kafka(sample_record, mock_producer)
 
     mock_producer.produce.assert_called_once_with(
         topic="crypto-prices", value='{"spam": "eggs"}'
@@ -69,18 +69,18 @@ async def test_send_record_to_kafka():
 
 @pytest.mark.asyncio
 async def test_send_record_to_kafka_error_handling():
-    mock_logger = Mock()
-    mock_producer = Mock()
-    mock_producer.produce.side_effect = KafkaException("Test KafkaException")
+    with unittest.mock.patch("kafka.producer.logger") as mock_logger:
+        mock_producer = Mock()
+        mock_producer.produce.side_effect = KafkaException("Test KafkaException")
 
-    sample_record = {"spam": "eggs"}
+        sample_record = {"spam": "eggs"}
 
-    await main._send_record_to_kafka(sample_record, mock_producer)
+        await service._send_record_to_kafka(sample_record, mock_producer)
 
-    mock_producer.produce.assert_called_once_with(
-        topic="crypto-prices", value='{"spam": "eggs"}'
-    )
+        mock_producer.produce.assert_called_once_with(
+            topic="crypto-prices", value='{"spam": "eggs"}'
+        )
 
-    mock_logger.error.assert_called_once_with(
-        "Error producing message to Kafka: Test KafkaException"
-    )
+        mock_logger.error.assert_called_once_with(
+            "Error producing message to Kafka: Test KafkaException"
+        )
