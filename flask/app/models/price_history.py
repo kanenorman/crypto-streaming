68e7a30303af98c500920fbb892b08ef5beafd10
kanenorman <kanenorman1996@gmail.com>
2024-01-19 18:59:03 -0600

    Apply black formatting

--- flask/app/models/price_history.py
+++ flask/app/models/price_history.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
 from datetime import datetime
 
-from ..extensions import database as db
-
 from flask_sqlalchemy import PrimaryKeyConstraint
+
+from ..extensions import database as db
 
 
 @dataclass()
@@ -11,11 +11,9 @@ class PriceHistory(db.Model):
     __tablename__ = "price_history"
 
     price: float = db.Column(db.Float(), nullable=False)
-    exchange : str = db.Column(db.String(255), nullable=False)
+    exchange: str = db.Column(db.String(255), nullable=False)
     trading_pair: str = db.Column(db.String(255), nullable=False)
-    at_time : datetime = db.Column(db.DateTime(), nullable=False)
+    at_time: datetime = db.Column(db.DateTime(), nullable=False)
     volume: float = db.Column(db.Float(), nullable=False)
 
-    __table_args__ = (
-        PrimaryKeyConstraint('exchange', 'trading_pair', 'at_time'),
-    )
+    __table_args__ = (PrimaryKeyConstraint("exchange", "trading_pair", "at_time"),)
